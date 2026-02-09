8fa9a1f694792c558eba0af5f47193c99d545030
FernanPC18 <231477850+FernanPC18@users.noreply.github.com>
2026-02-09 12:44:05 +0100

    feat(dni.py): Añadir función es_letra_valida

--- src/dni.py
+++ src/dni.py
@@ -17,3 +17,10 @@ class Dni:
         def es_numero_personal_valido(self) -> bool:
             """Comprueba que la parte numérica tiene 8 dígitos y solo contiene números."""
             return self.numero_personal.isdigit() and len(self.numero_personal) == 8
+
+        def es_letra_valida(self) -> bool:
+            """Comprueba que la letra está permitida en la tabla de asignación."""
+            return (
+                self.letra_asignacion.isalpha()
+                and self.tabla_asignacion.isLetraPermitida(self.letra_asignacion)
+            )
