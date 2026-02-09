79c473d20a0c4210257e0584026c7580d523b60e
FernanPC18 <231477850+FernanPC18@users.noreply.github.com>
2026-02-09 12:42:18 +0100

    feat(dni.py): Añadir función es_numero_personal_valido

--- src/dni.py
+++ src/dni.py
@@ -2,15 +2,18 @@ from tabla_asignacion import TablaAsignacion
 
 
 class Dni:
-
     def __init__(self, dni: str):
         self.dni_original = dni.strip()
         self.tabla_asignacion = TablaAsignacion()
 
-        dni_sano = self.dni_original.replace('-', '').replace(' ', '').upper()
+        dni_sano = self.dni_original.replace("-", "").replace(" ", "").upper()
         if len(dni_sano) == 0:
-            self.numero_personal = ''
-            self.letra_asignacion = ''
+            self.numero_personal = ""
+            self.letra_asignacion = ""
         else:
             self.numero_personal = dni_sano[:-1]
             self.letra_asignacion = dni_sano[-1]
+
+        def es_numero_personal_valido(self) -> bool:
+            """Comprueba que la parte numérica tiene 8 dígitos y solo contiene números."""
+            return self.numero_personal.isdigit() and len(self.numero_personal) == 8
